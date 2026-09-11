b6f72347ea45e6cc1f0c729b33dfc23b06709f5f
Assales Banoune <banoune.assales2@gmail.com>
2026-09-11 13:04:13 +0200

    etape3

---
+++ jeu.c
@@ -0,0 +1,9 @@
+#include "puissance4.h"
+#include <stdio.h>
+void afficherRegles(void){
+    printf("--------PUISSANCE4---------\n");
+    printf("Alignez quatre jetons horizontalement, verticalement ou en diagonale.\n");
+    printf("Les colonnes sont numerotees de 1 a 7.");
+
+
+}
---
+++ main.c
@@ -0,0 +1,7 @@
+#include "puissance4.h"
+#include <stdio.h>
+int main(void)
+{
+    afficherRegles();
+    return 0;
+}
