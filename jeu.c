052eec4b8fbadda0f6b527d17918880592ff6cad
Assales Banoune <banoune.assales2@gmail.com>
2026-09-16 09:24:23 +0200

    projet1

--- jeu.c
+++ jeu.c
@@ -4,6 +4,43 @@ void afficherRegles(void){
     printf("--------PUISSANCE4---------\n");
     printf("Alignez quatre jetons horizontalement, verticalement ou en diagonale.\n");
     printf("Les colonnes sont numerotees de 1 a 7.");
+}
+//Étape 4
+int colonneValide(int colonne){
+    return (colonne >= 1 && colonne <= 7);
+}
+//Étape 5
+int demanderColonne(void){
+    int colonne;
+    printf("Choisissez une colonne 1-7 : ");
+    scanf("%d", &colonne);
+    while(colonneValide(colonne) == 0){
+        printf("Colonne invalide. Choisissez une colonne (1-7) : ");
+        scanf("%d", &colonne);
+    }
+    return colonne;
+}
+//Étape 6 
+int changerJoueur(int joueur){
+    if(joueur == 1){
+        return 2;
+    }
+    else{
+        return 1;
+    }
+}
+void jouerPrototype(void) {
+    int joueurCourant = 1;
+    int i;
 
+    for (i = 0; i < 6; i++) {
+        printf("Tour du joueur %d\n", joueurCourant);
 
+        int colonne = demanderColonne();
+
+        printf("Le joueur %d choisit la colonne %d.\n",
+               joueurCourant, colonne);
+
+        joueurCourant = changerJoueur(joueurCourant);
+    }
 }
--- puissance4.h
+++ puissance4.h
@@ -5,5 +5,9 @@
  int demanderColonne(void);//type de retour:int
  int changerJoueur(int joueur);//type de retour:int
  void jouerPrototype(void);//y aura pas de retour
+ int colonneValide(int colonne);
+ int demanderColonne(void);
+ int changerJoueur(int joueur);
+ void jouerPrototype(void);
 #endif
 //pourquoi afficherRegles ne reçoit aucun paramètre et ne renvoie aucune valeur:afficherRegles() ne prend aucun parametre car les regles sont deja definies dans le programme. Elle ne retourne rien (void) car elle sert simplement a afficher les regles a l'ecran avec des prinf
